c27d9b99b8a4b8ddc26c8a628528c8254910d7ac
unknown <anthony_rosati@yahoo.com>
2023-12-26 01:40:38 -0600

    Created index.js in models folder, established relationships between models

---
+++ models/index.js
@@ -0,0 +1,22 @@
+// Import models
+const Blogpost = require('./Blogpost');
+const User = require('./User');
+const Comment = require('./Comment');
+
+// Users have many Blogposts
+User.hasMany(Blogpost, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+// Blogposts have many Comments
+Blogpost.hasMany(Comment, {
+    foreignKey: 'blogpost_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = {
+    Blogpost,
+    User,
+    Comment
+};
