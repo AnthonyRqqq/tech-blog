658613f777d7732b063324b461364121472198ce
unknown <anthony_rosati@yahoo.com>
2023-12-26 01:04:49 -0600

    Added connection.js to config folder, added code for connecting to sequelize

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+// For setting up sequelize connection
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3302,
+    }
+);
+
+module.exports = sequlize;
