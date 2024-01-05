0d88206ad581552d0b1d5a6fccc3098985ef8658
unknown <anthony_rosati@yahoo.com>
2024-01-04 18:30:16 -0600

    Added route to view specific blogposts by id

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -16,12 +16,39 @@ router.get('/', async (req, res) => {
 
         // Serializes data for sequelize
         const blogposts = dbBlogpostData.map((blogpost) =>
-        blogpost.get({ plain: true })
+            blogpost.get({ plain: true })
         );
 
         // Sends data to homepage.handlebars
         res.render('homepage', {
             blogposts
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json(err);
+    }
+});
+
+// Route for specific blogpost
+// Route looks like "url"/blogpost/:id
+router.get('/blogpost/:id', async (req, res) => {
+    try {
+        // Finds blogpost by id primary key
+        const dbBlogpostData = await Blogpost.findByPk(req.params.id, {
+            // Includes comments associated with the blogpost
+            include: [
+                {
+                    model: Comment,
+                },
+            ],
+        });
+
+        // Serializes data
+        const blogpost = dbBlogpostData.get({ plain: true });
+
+        // Sends data to blogpost.handlebars
+        res.render('blogpost', {
+            blogpost,
         });
     } catch (err) {
         console.error(err);
