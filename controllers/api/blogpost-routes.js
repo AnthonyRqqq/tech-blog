aa38c2b39ed6a384b453bc0cbcba5e67a6e6cd5d
unknown <anthony_rosati@yahoo.com>
2024-01-04 19:01:02 -0600

    Added code to update blogposts and delete blogposts by id

--- controllers/api/blogpost-routes.js
+++ controllers/api/blogpost-routes.js
@@ -18,3 +18,46 @@ router.post('/', async (req, res) => {
     }
 });
 
+// For updating a blogpost
+// url: /api/blogpost/:id
+router.put('/:id', async (req, res) => {
+    try {
+        const dbBlogpostData = await Blogpost.update(
+            {
+                title: req.body.title,
+                content: req.body.content,
+            },
+            {
+                where: {
+                    id: req.params.id
+                }
+            }
+        );
+
+        console.log('Update successful!');
+        res.json(dbBlogpostData);
+    } catch (err) {
+        console.error(err);
+        res.json(err);
+    }
+});
+
+// For deleting a blogpost
+// url: /api/blogpost/:id
+router.delete('/:id', async (req, res) => {
+    try {
+        const dbBlogpostData = await Blogpost.destroy(
+            {
+                where: {
+                    id: req.params.id
+                }
+            }
+        );
+
+        console.log('Successful deletion!');
+        res.json(dbBlogpostData);
+    } catch (err) {
+        console.error(err);
+        res.json(err)
+    };
+});
